2d9e652d50c5c24e4c70837ded2f77ed0180f5cf
zgy <zhuguangyang@zgy.local>
2016-10-10 18:05:33 +0800

    //

--- GYPhotoBrowser/GYPhotoBrowser/ViewController.swift
+++ GYPhotoBrowser/GYPhotoBrowser/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func localImages(_ sender: AnyObject) {
+        
+        let localImage = LocalImagesViewController()
+        localImage.imageType = LocalImagesViewController.PhotoType.Local
+        
+        navigationController?.pushViewController(localImage, animated: true)
+        
+        
+    }
+    
+    
+    @IBAction func netWorkImages(_ sender: AnyObject) {
+        
+        let localImage = LocalImagesViewController()
+        localImage.imageType = LocalImagesViewController.PhotoType.NetWork
+        
+        navigationController?.pushViewController(localImage, animated: true)
+        
+    }
+    
+    
+    @IBOutlet weak var netWorkImages: UIButton!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
